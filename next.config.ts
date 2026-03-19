d25991d184f7e69c04c42e2dd00c9a8ceb22df7b
Daniel Burton <danielburton2@sky.com>
2026-03-19 23:29:59 +0000

    Fixed merge conflict in next config

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,10 @@
 const nextConfig = {
 
   typescript: {
-
     /* allow builds even with TS errors */
-
     ignoreBuildErrors: true,
-
-  },
-
-<<<<<<< HEAD
- 
-=======
-  eslint: {
-
-    /* prevent eslint from stopping builds */
-
-    ignoreDuringBuilds: true,
-
   }
 
->>>>>>> d80cabaf8de6025d187f0b7eccf894f4bdbf9f43
 }
 
 export default nextConfig
